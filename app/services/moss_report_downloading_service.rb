e750ea87aae9167e738ec8d2461eb60ff9d6552f
Dexter Leng <dexterlengcs@gmail.com>
2021-01-03 19:22:03 +0800

    MossReportDownloadingService - do not zip report

--- app/services/moss_report_downloading_service.rb
+++ app/services/moss_report_downloading_service.rb
@@ -15,11 +15,8 @@ class MossReportDownloadingService
         --page-requisites \
         -m #{result_url_with_backslash}
     "
-    CommandExecutor.instance.execute!(cmd, chdir: temp_dir)
-    zip_folder(src: temp_dir, dst: dst_report)
+    CommandExecutor.instance.execute!(cmd, chdir: dst_report)
     nil
-  ensure
-    delete_temp_dir
   end
 
   private
@@ -33,22 +30,5 @@ class MossReportDownloadingService
       url += '/' unless url.ends_with?('/')
       url
     end
-  end
-
-  def temp_dir
-    @temp_dir ||= Dir.mktmpdir('mossu_report_downloading_service_')
-  end
-
-  def zip_folder(src:, dst:)
-    absolute_dst = File.expand_path(dst)
-    CommandExecutor.instance.execute!(
-      "cd #{src} && zip -r #{absolute_dst} ."
-    )
-  end
-
-  def delete_temp_dir
-    CommandExecutor.instance.execute!(
-      "rm -rf #{temp_dir}"
-    )
   end
 end

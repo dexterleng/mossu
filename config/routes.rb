395cff516e600449ecab07c9fe2f942fb08c3f81
Dexter Leng <dexterlengcs@gmail.com>
2021-01-06 22:48:10 +0800

    Generate User model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :checks, only: [:index, :show, :create] do
     get 'report', to: 'checks#report'

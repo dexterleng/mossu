42566e6f90b03c39297478c55148fe19e19501fe
Dexter Leng <dexterlengcs@gmail.com>
2021-01-06 22:55:57 +0800

    Generate knock controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
+  post 'user_token' => 'user_token#create'
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :checks, only: [:index, :show, :create] do

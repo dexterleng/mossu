87ea2d4ffb8bf44b7caaf16b0d9a0cf1dbc379a6
Dexter Leng <dexterlengcs@gmail.com>
2021-01-06 23:05:38 +0800

    Add sign up API

--- app/controllers/user_token_controller.rb
+++ app/controllers/user_token_controller.rb
@@ -1,2 +1,3 @@
 class UserTokenController < Knock::AuthTokenController
+  skip_before_action :verify_authenticity_token, raise: false
 end
--- config/initializers/knock.rb
+++ config/initializers/knock.rb
@@ -1,5 +1,6 @@
 Knock.setup do |config|
   config.token_lifetime = 1.day
+  config.token_secret_signature_key = -> { Rails.application.credentials.secret_key_base }
 
   ## Expiration claim
   ## ----------------

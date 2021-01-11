190951ab070b2d33a93d09a3a118643645f12d9b
Dexter Leng <dexterlengcs@gmail.com>
2021-01-11 16:17:00 +0800

    Validate zip file sizes

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -2,7 +2,7 @@ class Submission < ApplicationRecord
   has_one_attached :zip_file
   belongs_to :check
 
-  validate :zip_file_is_present
+  validate :zip_file_is_present, :zip_file_max_size
 
   def zip_file_is_present
     unless zip_file.attached?
@@ -10,5 +10,12 @@ class Submission < ApplicationRecord
       return
     end
     error.add(:zip_file, 'must be zip') unless zip_file.content_type == 'application/zip'
+  end
+
+  def zip_file_max_size
+    max_bytes = 5 * 1000 * 1000
+    if zip_file.attached? && zip_file.blob.byte_size > max_bytes
+      errors.add(:zip_file, 'Submission cannot exceed 5 MB')
+    end
   end
 end

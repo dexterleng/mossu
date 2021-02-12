e4698eed170e38e57bb0d5a690e175cbfe9d7454
Dexter Leng <dexterlengcs@gmail.com>
2021-02-12 20:23:28 +0800

    Track StartCheckJob

--- app/jobs/start_check_job.rb
+++ app/jobs/start_check_job.rb
@@ -2,14 +2,22 @@ class StartCheckJob < ApplicationJob
   queue_as :default
 
   def perform(check_id)
-    check = Check.find(check_id)
-    begin
-      check.transition_to_active
-      StartCheckService.new(check).perform
-      check.transition_to_completed
-    rescue StandardError => e
-      check.transition_to_failed
-      raise e
+    stats_service.track('check') do
+      check = Check.find(check_id)
+      begin
+        check.transition_to_active
+        StartCheckService.new(check).perform
+        check.transition_to_completed
+      rescue StandardError => e
+        check.transition_to_failed
+        raise e
+      end
     end
+  end
+
+  private
+
+  def stats_service
+    @stats_service ||= StatsService.new(namespace: 'sidekiq')
   end
 end

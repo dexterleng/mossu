ca96eb3a198cfcda4c826a6eaacec344581d1744
Dexter Leng <dexterlengcs@gmail.com>
2021-03-01 16:39:21 +0800

    Add authenticated public sign up url

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  include Knock::Authenticable
+  before_action :authenticate_user
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   # GET /users
@@ -24,14 +26,17 @@ class UsersController < ApplicationController
   # POST /users
   # POST /users.json
   def create
-    @user = User.new(user_params)
+    @user = User.new(
+      params.require(:user).permit(:username, :email, :password, :password_confirmation)
+    )
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
+        # format.html { redirect_to @user, notice: 'User was successfully created.' }
+        # format.json { render :show, status: :created, location: @user }
+        format.json { render json: {}, status: :created }
       else
-        format.html { render :new }
+        # format.html { render :new }
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
